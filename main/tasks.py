7fa1b9971e4a060172b116340baa1e86c4c5e2f1
Dmytro Shevchenko <dimashevchenko10021999@gmail.com>
2020-03-20 01:14:41 +0200

    Create task send_beat_email
    :Release Notes:
    
    :Detailed Notes:
    
    :Testing Performed:
    No tests
    
    :QA Notes:
    
    :Issues Addressed:
    
    Signed-off-by: Shevchenko Dmytro <dimashevchenko10021999@gmail.com>

--- main/tasks.py
+++ main/tasks.py
@@ -8,3 +8,15 @@ from .models import Contact
 @app.task
 def send_spam_email(user_email):
     send(user_email)
+
+
+@app.task
+def send_beat_email():
+    for contact in Contact.objects.all():
+        send_mail(
+            'Вы подписались на рассылку',
+            'Мы будем присылать Вам много спама каждые 1 мин !',
+            'djangos99@gmail.com',
+            [contact.email],
+            fail_silently=False,
+        )
--- send_email_celery_django/celery.py
+++ send_email_celery_django/celery.py
@@ -7,3 +7,11 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'send_email_celery_django.settin
 app = Celery('send_email_celery_django')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
+
+app.conf.beat_schedule = {
+    'send-spam-every-5-minute': {
+    'task': 'main.tasks.send_beat_email',
+    'schedule': crontab(minute='*/5'),
+    }
+}
