f2e37bc9d503718306bd0f1c8cc5f9c31f81526b
Dmytro Shevchenko <dimashevchenko10021999@gmail.com>
2020-03-21 11:43:30 +0200

    Celery monitoring tool
    :Release Notes:
    
    :Detailed Notes:
    
    :Testing Performed:
    No tests
    
    :QA Notes:
    
    :Issues Addressed:
    
    Signed-off-by: Shevchenko Dmytro <dimashevchenko10021999@gmail.com>

--- main/tasks.py
+++ main/tasks.py
@@ -20,3 +20,24 @@ def send_beat_email():
             [contact.email],
             fail_silently=False,
         )
+
+@app.task
+def my_task(a, b):
+    c = a + b
+    return c
+
+@app.task
+def my_task_as(d, e):
+    c = d + e
+    return c
+
+@app.task(bind=True, default_retry_delay=5*60)
+def my_task_retry(self, x, y):
+    try:
+        return x + y
+    except Exception as exc:
+        raise self.retry(exc=exc, countdown=60)
+
+#@shared_task()
+#def my_sh_task(msg):
+#    return msg + "!!!"
